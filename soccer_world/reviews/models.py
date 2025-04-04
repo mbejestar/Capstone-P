c229e2e63c5c58c6e4d36ab9a49cdae6fae8f0f0
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 13:35:54 +0200

    API

--- soccer_world/reviews/models.py
+++ soccer_world/reviews/models.py
@@ -7,6 +7,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)  
     rating = models.IntegerField()  
     comment = models.TextField()  
+    created_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):  
         return f'Review for {self.team.name} by {self.user.username}'  
