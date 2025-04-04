f643c3f1073edcaabfcabcf28b5622d76bbb5f60
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 23:03:17 +0200

    front end soccer world

--- soccer_world/teams/views.py
+++ soccer_world/teams/views.py
@@ -1,10 +1,17 @@
-# teams/views.py  
-from rest_framework import viewsets  
-from rest_framework.permissions import IsAuthenticatedOrReadOnly  
+ from rest_framework import views  
+from rest_framework.response import Response  
 from .models import Team  
-from .serializers import TeamSerializer  
 
-class TeamViewSet(viewsets.ModelViewSet):  
-    queryset = Team.objects.all()  
-    serializer_class = TeamSerializer  
-    permission_classes = [IsAuthenticatedOrReadOnly]  
+class TeamStatisticsView(views.APIView):  
+    permission_classes = [permissions.AllowAny]  
+
+    def get(self, request):  
+        team_stats = []  
+        for team in Team.objects.all():  
+            stats = {  
+                'name': team.name,  
+                'average_rating': team.average_rating(),  
+                'number_of_reviews': team.number_of_reviews(),  
+            }  
+            team_stats.append(stats)  
+        return Response(team_stats)  
