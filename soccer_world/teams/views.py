e6fb3fa9452a4e0e5713f95f50a2aa7d9cfe8f45
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 23:23:42 +0200

    front end soccer world

--- soccer_world/teams/views.py
+++ soccer_world/teams/views.py
@@ -1,9 +1,11 @@
- from rest_framework import views  
-from rest_framework.response import Response  
-from .models import Team  
+from rest_framework import views, viewsets, permissions
+from rest_framework.response import Response
+from django_ratelimit.decorators import ratelimit
+from .models import Team
+from .serializers import TeamSerializer
 
 class TeamStatisticsView(views.APIView):  
-    permission_classes = [permissions.AllowAny]  
+    permission_classes = [permissions.AllowAny] 
 
     def get(self, request):  
         team_stats = []  
@@ -15,3 +17,28 @@ class TeamStatisticsView(views.APIView):
             }  
             team_stats.append(stats)  
         return Response(team_stats)  
+
+class TeamViewSet(viewsets.ModelViewSet):  
+    queryset = Team.objects.all()  
+    serializer_class = TeamSerializer  
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  
+
+    @ratelimit(key='ip', rate='5/m', method='ALL', block=True)  
+    def list(self, request, *args, **kwargs):  
+        return super().list(request, *args, **kwargs)  
+
+    @ratelimit(key='ip', rate='5/m', method='ALL', block=True)  
+    def retrieve(self, request, *args, **kwargs):  
+        return super().retrieve(request, *args, **kwargs)  
+
+    @ratelimit(key='ip', rate='5/m', method='ALL', block=True)  
+    def create(self, request, *args, **kwargs):  
+        return super().create(request, *args, **kwargs)  
+
+    @ratelimit(key='ip', rate='5/m', method='ALL', block=True)  
+    def update(self, request, *args, **kwargs):  
+        return super().update(request, *args, **kwargs)  
+
+    @ratelimit(key='ip', rate='5/m', method='ALL', block=True)  
+    def destroy(self, request, *args, **kwargs):  
+        return super().destroy(request, *args, **kwargs)  
