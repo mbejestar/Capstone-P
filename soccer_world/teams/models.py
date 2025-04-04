8151f60bc16ee66ab8705664484f63254701019b
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 13:34:18 +0200

    Update models.py

--- soccer_world/teams/models.py
+++ soccer_world/teams/models.py
@@ -5,6 +5,8 @@ class Team(models.Model):
     city = models.CharField(max_length=100)  
     league = models.CharField(max_length=100)  
     description = models.TextField()  
+    founded_year = models.IntegerField()
+    logo = models.ImageField(upload_to='team_logos/')
 
     def __str__(self):  
         return self.name  
