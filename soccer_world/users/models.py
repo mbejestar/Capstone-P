aa2b908aeb2ba73b80d63612936d775ac6f8eda7
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 23:06:01 +0200

    front end soccer world

--- soccer_world/users/models.py
+++ soccer_world/users/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import User  
+from django.db import models  
 
-# Create your models here.
+class UserProfile(models.Model):  
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    is_new_user = models.BooleanField(default=True)  # To track if a user is new  
