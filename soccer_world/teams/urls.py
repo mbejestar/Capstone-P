0575cb5a1eada668699cb8b2e1c1be22cfbba6b5
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-04 23:04:53 +0200

    front end soccer world

--- soccer_world/teams/urls.py
+++ soccer_world/teams/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include  
 from rest_framework.routers import DefaultRouter  
 from .views import TeamViewSet  
+from .views import TeamStatisticsView  
 
 router = DefaultRouter()  
 router.register(r'teams', TeamViewSet)  
 
 urlpatterns = [  
+    path('statistics/', TeamStatisticsView.as_view(), name='team-statistics'),  
     path('', include(router.urls)),  
 ]  
