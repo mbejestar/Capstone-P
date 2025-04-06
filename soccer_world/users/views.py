e8b438ef8bb45ef8ba27bb35f3fc144f5e596a11
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-04-06 20:44:14 +0200

    soccer world views

--- soccer_world/users/views.py
+++ soccer_world/users/views.py
@@ -22,4 +22,9 @@ class CustomAuthToken(ObtainAuthToken):
         user = serializer.validated_data['user']  
         token, created = Token.objects.get_or_create(user=user)  
 
-        return Response({'token': token.key})  
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })  
+    
